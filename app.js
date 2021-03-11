d7d32dbe5cf47d1ec7b1e61858e486b1a68d9620
lanagrace <lapericic@gmail.com>
2021-03-11 11:36:37 +0000

    Adding a post request /submit

--- app.js
+++ app.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const app = express();
 
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ //this packages incoming/outcoming data as json
+    extended: false
+}));//this monitors data incoming on the url
+
 //Express Route - This returns the message 'Home Page' when a GET request is made to the Server on the / route
 app.get('/', (req, res) =>{
     res.send('Home Page')
@@ -10,6 +16,11 @@ app.get('/', (req, res) =>{
 //Express Route - This returns the message 'About Page' when a GET request is made to the Server on the /about route
 app.get('/about', (req, res)=>{
     res.send('About Page');
+})
+
+app.post('/submit', (req, res)=> {
+    console.log(req.body);
+    res.send('Success');
 })
 
 //Listening for requests on localhost:3000
